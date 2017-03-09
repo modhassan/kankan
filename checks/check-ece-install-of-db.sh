cbd46a30c9b7e963dccfd61fee37e0badf2664a4
Torstein Krause Johansen <torstein@escenic.com>
2017-03-09 10:27:50 +0100

    New checks for all tables for all installed plugins
    - refactored mysql SQL queries out to _run_sql_mysql

--- checks/check-ece-install-of-db.sh
+++ checks/check-ece-install-of-db.sh
@@ -18,5 +18,48 @@ check_ece_install_have_installed_db_files() {
       echo -n "."
     fi
   done
+}
 
+_run_sql_mysql() {
+  local sql=$1
+
+  mysql \
+    -u "${ece_db_user}" \
+    -p"${ece_db_password}" "${ece_db_schema}" \
+    -e "${sql}" &> /dev/null
+}
+
+check_ece_install_have_set_up_ece_db_tables() {
+  if [[ -n "${ece_db_user}" &&
+          -n "${ece_db_password}" &&
+          -n "${ece_db_schema}" ]]; then
+    local sql='
+      select count(contentID) from Content;
+    '
+    _run_sql_mysql "${sql}" || {
+      flag_error "Should have set up DB with schema ${ece_db_schema} on ${HOSTNAME}"
+    }
+  fi
+}
+
+check_ece_install_have_set_up_plugin_db_tables() {
+  find /usr/share/escenic -name "escenic-*" -type d |
+    while read -r plugin_dir; do
+      local plugin_tables_sql="${plugin_dir}/misc/database/mysql/tables.sql"
+
+      if [ ! -r "${plugin_tables_sql}" ]; then
+        continue
+      fi
+
+      local plugin=${plugin_dir##/usr/share/escenic/escenic-}
+
+      grep -i "create table" "${plugin_tables_sql}" |
+        awk '{print $3}' |
+        while read -r table; do
+          _run_sql_mysql "describe ${table}" || {
+            flag_error "Table ${table} from plugin ${plugin} should be present" \
+                       "in schema ${ece_db_schema} on ${HOSTNAME}"
+          }
+        done
+    done
 }
