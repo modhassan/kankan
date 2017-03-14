c1190bbac032600e5c13b24711bd272ade32d7b0
Torstein Krause Johansen <torstein@escenic.com>
2017-03-14 13:56:56 +0100

    Added simple check that at least one publication exists
    - uses /usr/bin/ece to query ECE so this means that the publication
      must exist in the DB to be deemed as valid.

---
+++ checks/check-ece-install-of-publication.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+check_ece_install_have_installed_publication() {
+  local required_count=1
+  local ece_user=escenic
+
+  local publication_count=0
+  publication_count=$(
+    su - "${ece_user}" -c "ece -i engine1 list-publications" |
+      sed '/These are all the/d' |
+      wc -l)
+
+  if [ "${publication_count-0}" -lt "${required_count}" ]; then
+    flag_error "At least ${required_count} publications should be available."
+  fi
+}
