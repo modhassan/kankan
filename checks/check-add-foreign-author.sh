6cc65ff25966bbc7d9337768d55748a8ec54a22c
Torstein Krause Johansen <torstein@escenic.com>
2017-04-11 12:07:56 +0200

    refactoring

--- checks/check-add-foreign-author.sh
+++ checks/check-add-foreign-author.sh
@@ -10,7 +10,6 @@ _foreign_create_content_item() {
   local user_and_password=
 
 
-
   for el in "${!ece_instance_host_port_and_http_auth_map[@]}"; do
     user_and_password=$el
     title=$(fortune | head -n 1)
@@ -34,8 +33,6 @@ _foreign_create_content_item() {
         xmllint --xpath "${xpath}" - |
         sed -r 's#.*["](.*)["].*#\1#')
 
-    echo sub_section_uri=$sub_section_uri
-
     local root_section_uri=
     xpath='/feed/entry/link[@rel="http://www.vizrt.com/types/relation/content-items"]/@href'
     root_section_uri=$(
@@ -48,8 +45,6 @@ _foreign_create_content_item() {
         sed 's#xmlns=".*"##' |
         xmllint --xpath "${xpath}" - |
         sed -r 's#.*["](.*)["].*#\1#')
-
-    echo root_section_uri=$root_section_uri
 
     cat > "${tmp_file}" <<EOF
 <?xml version="1.0"?>
@@ -73,47 +68,66 @@ _foreign_create_content_item() {
 EOF
 
     local content_item_url=
-    content_item_url=$(
-      curl --include \
-           --user "${http_auth}" \
-           --request POST \
-           --header "Content-Type: application/atom+xml; type=entry" \
-           --upload-file "${tmp_file}" \
-           "${root_section_uri}" 2>&1 |
-        sed -n -r 's#Location: ([a-z0-9]*)#\1#p'
-                    )
-    echo content_item_url="$content_item_url"
-
+    content_item_url=$(_foreign_create_content_item_from_xml "${tmp_file}" "${root_section_uri}")
     # We get some nonsense character at the end because of standard
     # error/out redirect. Removing it here.
     end=$(( ${#content_item_url} - 1 ))
-    echo end=$end
 
     curl \
+      --silent \
       --user "${http_auth}" \
-      "${content_item_url:0:${end}}" |
-      xmllint --format - > "${tmp_file}"
+      "${content_item_url:0:${end}}" \
+      > "${tmp_file}"
 
-    xmlstarlet \
-      ed -P --inplace \
-      -N a="http://www.w3.org/2005/Atom" \
-      -s /a:entry -t elem -n TMP -v "" \
-      -r /a:entry/TMP -v author \
-      -s /a:entry/author -t elem -n name -v "$(_foreign_random_author_name)" \
-      -s /a:entry/author -t elem -n uri -v "$(_foreign_random_author_uri)" \
-      "${tmp_file}"
-
-    cat "${tmp_file}"
-
-    # TODO PUT this back
-
-
+    _foreign_add_random_author_to_content_item \
+      "${tmp_file}" \
+      "${content_item_url:0:${end}}"
   done
 
   # cat "${tmp_file}"
 
   rm -rf "${tmp_file}"
 
+}
+
+_foreign_create_content_item_from_xml() {
+  curl \
+    --silent \
+    --include \
+    --user "${http_auth}" \
+    --request POST \
+    --header "Content-Type: application/atom+xml; type=entry" \
+    --upload-file "${tmp_file}" \
+    "${root_section_uri}" 2>&1 |
+    sed -n -r 's#Location: ([a-z0-9]*)#\1#p'
+}
+
+_foreign_add_random_author_to_content_item() {
+  local tmp_file=$1
+  local content_item_uri=$2
+
+  _foreign_add_random_author_to_xml "${tmp_file}"
+  curl \
+    --silent \
+    --user "${http_auth}" \
+    --request PUT \
+    --header "If-Match: *" \
+    --header "Content-Type: application/atom+xml; type=entry" \
+    --upload-file "${tmp_file}" \
+    "${content_item_uri}"
+}
+
+_foreign_add_random_author_to_xml() {
+  local tmp_file=$1
+
+  xmlstarlet \
+    ed -P --inplace \
+    -N a="http://www.w3.org/2005/Atom" \
+    -s /a:entry -t elem -n TMP -v "" \
+    -r /a:entry/TMP -v author \
+    -s /a:entry/author -t elem -n name -v "$(_foreign_random_author_name)" \
+    -s /a:entry/author -t elem -n uri -v "$(_foreign_random_author_uri)" \
+    "${tmp_file}"
 }
 
 _foreign_random_author_name() {
