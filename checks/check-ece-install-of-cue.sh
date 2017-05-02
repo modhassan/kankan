52217b91bd1c563cfcec13e66bbc7b92c0e73a47
Torstein Krause Johansen <torstein@escenic.com>
2017-05-02 15:15:31 +0200

    Added integration test for CUE installation

---
+++ checks/check-ece-install-of-cue.sh
@@ -0,0 +1,31 @@
+# Emacs: -*- mode: sh; sh-shell: bash; -*-
+
+check_cue_app_config_has_been_generated() {
+  # Since we have more than one test here, we increase the counter
+  # ourselves in the loop below.
+  ((number_of_tests--))
+
+  for dir in $(find /etc/escenic/cue-web-* -maxdepth 0 -type d); do
+    local file="${dir}"/app.config.js
+    ((number_of_tests++))
+
+    if [ "${verbose-0}" -eq 1 ]; then
+      printf "%s\n" "${file} should have been created"
+    else
+      printf "%s" "."
+    fi
+
+    test -e "${file}" || {
+      flag_error "${file} should have been generated"
+    }
+  done
+}
+
+check_cue_available_on_port_80() {
+  local url=http://localhost:80/cue-web/
+
+  curl --silent --head "${url}" |
+    grep -w -q '200 OK' || {
+    flag_error "CUE web interface isn't available: ${url} "
+  }
+}
